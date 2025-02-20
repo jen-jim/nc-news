feebb944ec8ee7140cd115e3dbceea4cfa1ac310
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-02-20 19:58:06 +0000

    implement search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        navigate(`/topics/${searchTerm}`);
+        setSearchTerm("");
+    }
+
     return (
         <form className="search-bar-container">
             <input
                 className="search-input"
                 type="text"
+                value={searchTerm}
                 placeholder="Search topics"
+                onChange={(e) => setSearchTerm(e.target.value)}
+                required
             />
-            <button className="search-button">
+            <button
+                className="search-button"
+                type="submit"
+                onClick={handleSubmit}
+            >
                 <i className="fa-solid fa-magnifying-glass"></i>
             </button>
         </form>
