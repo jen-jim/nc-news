121865803c51adeb622459cee21408a9cc63b2af
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-02-20 12:35:04 +0000

    add delete button on logged user's comment cards

---
+++ src/components/RemoveComment.jsx
@@ -0,0 +1,7 @@
+export default function RemoveComment({ commentId }) {
+    return (
+        <button className="delete-button">
+            <i className="fa-solid fa-trash-can"></i>
+        </button>
+    );
+}
